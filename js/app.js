04d18ba95d62fcb20c440565e3bcb88e17edae83
Maria Westling <Maria@n181-p189.kthopen.kth.se>
2016-02-16 13:15:32 +0100

    Added indexView

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 $(function() {
 	//We instantiate our model
+	
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
+	var indexView = new IndexView($("#indexView"), model);
 
 	var dinnerOptionView = new DinnerOptionView($("#dinnerOptionView"), model);
+	
 	var dinnerOptionViewController = new DinnerOptionViewController(dinnerOptionView,model);
 
 	var allDishesView = new AllDishesView($("#allDishesView"), model);
