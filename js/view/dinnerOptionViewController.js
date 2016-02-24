2bf434fa05cc7bbf0b24fbbd0fa782915671dd2d
Riverbell <emba93@live.se>
2016-02-24 11:46:49 +0100

    Tog bort kommentarer

--- js/view/dinnerOptionViewController.js
+++ js/view/dinnerOptionViewController.js
@@ -2,13 +2,10 @@
 var DinnerOptionViewController = function(view, model, overallController) {
  
 	view.plusButton.click(function(){
-		console.log(view.plusButton);
-		console.log("plus click");
 		model.setNumberOfGuests(model.getNumberOfGuests() + 1);
 	});
  
 	view.minusButton.click(function(){
-		console.log("minus click")
 		model.setNumberOfGuests(model.getNumberOfGuests() - 1);
 	});
 
